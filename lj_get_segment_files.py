a6a7fc01383f1b8996333d1f318aa2afd0e52f51
Bjoern <bjohas+git@gmail.com>
2015-05-15 22:41:44 +0100

    Added an alternative naming convention for .html files.

--- lj_get_segment_files.py
+++ lj_get_segment_files.py
@@ -10,6 +10,9 @@ import ConfigParser
 from os.path import expanduser
 import os.path
 
+DEBUG = False
+HTML_FILE_LONG_NAME = False
+
 DATA_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                     "../jb_data")
 EPISODE_DIRECTORY = os.path.join(DATA_DIRECTORY, "Late_Junction")
@@ -17,6 +20,9 @@ EPISODE_DIRECTORY = os.path.join(DATA_DIRECTORY, "Late_Junction")
 EPISODE_FILE_PATTERN = "*.xml"
 SEGMENT_FILE_PATTERN = "*.html"
 NAMESPACE = "{http://linuxcentre.net/xmlstuff/get_iplayer}"
+
+def getboolean(mystring):
+  return mystring == "True"
 
 # Check for configuration files
 configfile = os.path.join( expanduser("~"), ".junctionbox")
@@ -27,6 +33,10 @@ if os.path.isfile(configfile):
         confitems = dict(Config.items('basic'))
         if 'data_directory' in confitems:
             DATA_DIRECTORY = confitems['data_directory']
+        if 'debug' in confitems:
+            DEBUG = getboolean(confitems['debug'])
+        if 'html_file_long_name' in confitems:
+            HTML_FILE_LONG_NAME = getboolean(confitems['html_file_long_name'])
 
 #TODO remove and search for subdirs instead
 EPISODE_DIRECTORY = os.path.join(DATA_DIRECTORY, "Late_Junction")
@@ -40,19 +50,28 @@ def get_segment_files():
 
     for episode in episodes:
         pid = episode['pid']
-        segment_file = os.path.join(EPISODE_DIRECTORY, pid + '.html')
+        if HTML_FILE_LONG_NAME:
+            fileprefix = episode['fileprefix']
+            segment_file = os.path.join(EPISODE_DIRECTORY, fileprefix + '.segments.html')
+        else: 
+            segment_file = os.path.join(EPISODE_DIRECTORY, pid + '.html')
         segment_data_file = os.path.join(EPISODE_DIRECTORY, pid + ".p")
         if not(os.path.isfile(segment_file)):
+            if DEBUG: 
+                print 'Downloading to: ' + segment_file
             segment_html = urllib.urlopen('http://www.bbc.co.uk/programmes/' + pid + '/segments').read()
             f = open(segment_file, 'w')
             f.write(segment_html)
             f.close()
         else:
+            if DEBUG: 
+                print 'Reusing: ' + segment_file
             f = open(segment_file, 'r')
             segment_html = f.read()
             f.close()
         if not(os.path.isfile(segment_data_file)):
-            print segment_data_file
+            if DEBUG: 
+                print "Creating: " + segment_data_file
             segment_data = parse_segment_html(segment_html)
             pickle.dump(segment_data, open(segment_data_file, "wb"))
 
@@ -115,8 +134,9 @@ def get_episodes():
         root = tree.getroot()
 
         pid = root.find(NAMESPACE + 'pid').text
+        fileprefix = root.find(NAMESPACE + 'fileprefix').text
 
-        episodes.append({'pid': pid})
+        episodes.append({'pid': pid, 'fileprefix': fileprefix})
 
     return episodes        
 
