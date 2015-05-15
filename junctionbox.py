ecbabab9c4e795cd649776c8683c8ea02f8ea875
Bjoern <bjohas+git@gmail.com>
2015-05-15 22:19:54 +0100

    Hiding cursor reverted.

--- junctionbox.py
+++ junctionbox.py
@@ -406,7 +406,7 @@ def main_loop(screen):
     global current_episode, episodes, stdscr, favourited_log_queue
 
     stdscr = screen    
-#    curses.curs_set(0)
+    curses.curs_set(0)
     curses.halfdelay(4)
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
