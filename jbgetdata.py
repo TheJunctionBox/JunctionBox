69279c786e1e4c2266db6042f0f25deaf40ffaf7
Bjoern <bjohas+git@gmail.com>
2015-06-15 19:01:20 +0100

    Switched track database to json, merging with playlist.json improved, and added interpolation for tracks listed without timings.

--- jbgetdata.py
+++ jbgetdata.py
@@ -22,6 +22,7 @@ EPISODE_DIRECTORY_LIST = "Late_Junction"
 
 JB_DATABASE = os.path.join(DATA_DIRECTORY, "JB_DATABASE" )
 JB_DATABASE_TRACKS = os.path.join(JB_DATABASE, "tracks" )
+#JB_DATABASE_USER   = os.path.join(JB_DATABASE, "user" )
 
 EPISODE_FILE_PATTERN = "*.xml"
 SEGMENT_FILE_PATTERN = "*.html"
@@ -65,14 +66,17 @@ def get_segment_files(episodes):
     #download missing segment files
     for episode in episodes:
         pid = episode['pid']
+        duration = episode['duration']
         # segment_data_file is kept in DB, segment_file is kept alongside episodes
-        segment_data_file = os.path.join(JB_DATABASE_TRACKS, pid + ".p")
+        segment_p_file = os.path.join(JB_DATABASE_TRACKS, pid + ".p")
+        segment_data_file = os.path.join(JB_DATABASE_TRACKS, pid + ".json")
+#        segment_user_file = os.path.join(JB_DATABASE_USER, pid + ".p")
         if HTML_FILE_LONG_NAME:
             segment_file = os.path.join(episode['dir'], episode['fileprefix'] + '.segments.html')
-            segment_json_file = os.path.join(episode['dir'], episode['fileprefix'] + '.playlist.json')
+            segment_playlist_file = os.path.join(episode['dir'], episode['fileprefix'] + '.playlist.json')
         else: 
             segment_file = os.path.join(episode['dir'], episode['pid'] + '.html')
-            segment_json_file = os.path.join(episode['dir'], episode['pid'] + '.json')
+            segment_playlist_file = os.path.join(episode['dir'], episode['pid'] + '.json')
         #print segment_file + " " + episode['dir'] + " " + pid
         # Retrieve segments html file:
         if not(os.path.isfile(segment_file)):
@@ -84,18 +88,17 @@ def get_segment_files(episodes):
             f.write(segment_html)
             f.close()
         # Retrieve segments json file:
-        if not(os.path.isfile(segment_json_file)):
+        if not(os.path.isfile(segment_playlist_file)):
             if DEBUG: 
-                print 'Downloading to: ' + segment_json_file
-            segment_json_data = urllib.urlopen('http://www.bbc.co.uk/programmes/' + pid + '/playlist.json').read()
+                print 'Downloading to: ' + segment_playlist_file
+            segment_playlist_string = urllib.urlopen('http://www.bbc.co.uk/programmes/' + pid + '/playlist.json').read()
             # Need to check that this worked, i.e. that segment_html is not empty!
-            f = open(segment_json_file, 'w')
-            f.write(segment_json_data)
+            f = open(segment_playlist_file, 'w')
+            f.write(segment_playlist_string)
             f.close()
         UPDATE_MODE = False
-        NEW_SEGMENT_DATA_MODE = False
-        # If there is no database entry, create one:
-        if not(os.path.isfile(segment_data_file)) or UPDATE_MODE or NEW_SEGMENT_DATA_MODE:
+        # If there is no database entry, create one:        
+        if not(os.path.isfile(segment_data_file)) or UPDATE_MODE:
             # Should only save this if segments were retrieved - see comment in parse_segment_html()
             if not(os.path.isfile(segment_data_file)):
                 if DEBUG: 
@@ -103,35 +106,25 @@ def get_segment_files(episodes):
                 f = open(segment_file, 'r')
                 segment_html = f.read()
                 f.close()
-                segment_data = parse_segment_html(segment_html)
+                segment_data = parse_segment_html(segment_html, duration)
             else:
-                segment_data = pickle.load(open(segment_data_file, "rb"))
-            if NEW_SEGMENT_DATA_MODE:
-                segment_data2 = parse_segment_html(segment_html)
-                segment_data = segment_data_merge(segment_data, segment_data2)
-            f = open(segment_json_file, 'r')
-            segment_json_data = f.read()
+                with open(segment_data_file, 'r') as outfile:
+                    segment_data = json.load(outfile)
+                    # segment_data = pickle.load(open(segment_data_file, "rb"))
+            f = open(segment_playlist_file, 'r')
+            segment_playlist_string = f.read()
             f.close()
-            segment_json = json.loads(segment_json_data)
+            segment_json = json.loads(segment_playlist_string)
             if 'allAvailableVersions' in segment_json:
                 if len(segment_json['allAvailableVersions']) > 0:
                     if DEBUG: 
                        print "\n--- Update: " + segment_data_file
                     playlist = segment_json['allAvailableVersions'][0]['markers']
-                    segment_data = data_merge(segment_data, playlist)
-            # Normalise: Should check that all required fields are there
-            for i in range(len(segment_data)):
-                if not('ends' in segment_data[i]):
-                    segment_data[i]['ends'] = -1
-                if not('start' in segment_data[i]):
-                    segment_data[i]['start'] = -1
-                if not('mystart' in segment_data[i]):
-                    segment_data[i]['mystart'] = -1
-            if (len(segment_data) > 0):
-                if (segment_data[0]['seconds'] == 0):
-                    segment_data[0]['seconds'] = 11;
+                    segment_data = data_merge(segment_data, playlist, pid)
             # Write to database:
-            pickle.dump(segment_data, open(segment_data_file, "wb"))
+            # pickle.dump(segment_data, open(segment_data_file, "wb"))
+            with open(segment_data_file, 'w') as outfile:
+                json.dump(segment_data, outfile, indent=4)
 
 def segment_data_merge(segment_data, segment_data2):
     if len(segment_data) != len(segment_data2):
@@ -141,13 +134,33 @@ def segment_data_merge(segment_data, segment_data2):
         pass
     return segment_data
 
-def data_merge(segment_data, playlist):
+def data_merge(segment_data, playlist,pid):
     if len(playlist) != len(segment_data):
         print "ERROR: len(playlist) != len(segment_data)"
+        print str(len(playlist))+ " " + str(len(segment_data))
+        # for i in range(len(playlist)):
+        #     print segment_data[i]['track'] + " --- " + playlist[i]['text']
+        # print segment_data[len(segment_data)-1]['track']
     if playlist != None:
         for i in range(len(segment_data)):
-            segment_data[i]['start'] = playlist[i]['start']
-            segment_data[i]['title'] = playlist[i]['text'] 
+            if i < len(playlist):
+                offset = 0
+                # Fixing an episode specific track counting issue:
+                if pid == 'b05w82wf' and i > 2:
+                    offset = 2
+                if i + offset < len(segment_data):
+                    if len(playlist) != len(segment_data):
+                        try:
+                            print segment_data[i+offset]['track'] + " --- " + playlist[i]['text']
+                        except:
+                            pass
+                    segment_data[i+offset]['start'] = playlist[i]['start']
+                    segment_data[i+offset]['title'] = playlist[i]['text'] 
+                    segment_data[i+offset]['id']    = playlist[i]['id'] 
+            else:
+                if pid != 'b05w82wf':
+                    print "No playlist data for index: "+str(i)
+                    print segment_data[i]['track']
 #             if (playlist[i]['text'] == segment_data[i]['track']):
 #                 try:
 #                     print str(i) + " OK " + format_time(playlist[i]['start'])+" "+ playlist[i]['text'].encode('utf-8').strip() + " = " + segment_data[i]['track'] 
@@ -169,26 +182,66 @@ def format_time(seconds):
 
     return str(mins).rjust(2, "0") + ":" + str(secs).rjust(2, "0")
 
-def parse_segment_html(html):
+def parse_segment_html(html, duration):
     # expression needs to be improved to capture additional data (cf. todo.txt). Also note that sometimes times aren't present, in which case <div[^>]*>(\d\d):(\d\d)<\/div> is missing.
     # Should maybe add "(?:....)?" but need to make sure that this doesn't cause problems down the line. Most likely problem with older episodes only.
-    expression = r'<div class="segment__track">\s*<div[^>]*>(\d\d):(\d\d)<\/div>.*?<span class="artist" [^>]*>([^<>]*)<\/span>.*?<span property="name">([^<>]*)<\/span>'
-#  + '(?:\s*<\/p>\s*(<ul.*?<\/ul>)?(\s*<ul.*?<\/ul>)?)?'
+    expression = r'<div class="segment__track">\s*(?:<div[^>]*>(\d\d):(\d\d)<\/div>)?.*?<span class="artist" [^>]*>([^<>]*)<\/span>.*?(?:<span|<p class="no-margin") property="name">([^<>]*)<\/(?:span|p)>(?:(?:\s*<\/p>)?\s*(<ul.*?<\/ul>)?(\s*<ul.*?<\/ul>)?)?'
 
     p = re.compile(expression)
 
     data = []
 
+    trackno = 0
     for m in p.finditer(html):    
-        loc = m.group(1) + ":" + m.group(2)
-        seconds = int(m.group(1)) * 3600 + int(m.group(2)) * 60
+        trackno =         trackno + 1
+        if m.group(1) != None:
+            loc = str(m.group(1)) + ":" + str(m.group(2))
+            seconds = int(m.group(1)) * 3600 + int(m.group(2)) * 60
+        else:
+            loc = ""
+            seconds = -1
         artist = m.group(3)
         track = m.group(4)
-        
-        #B: Tracks should have end-times. 
-        data.append({'loc':loc, 'seconds':seconds, 'artist':artist, 'track':track,'ends': -1, 'mystart': -1,
-                     'favourite':False, 'favourited':False, 'heard':False})
+        contributors = str(m.group(5))
+        etc = str(m.group(6))
 
+        if etc == "" and re.match( r'contributor' , contributors):
+            etc = contributors
+            contributors = ""
+        contributors = re.sub(r'<.*?>', '', contributors)
+        etc = re.sub(r'<.*?>', '', etc)
+        
+        # data stores: (1) The extracted information, (2) Empty fields for inf appended from playlist.json, (3) user data
+        data.append({'number': trackno, 'loc':loc, 'seconds':seconds, 'artist':artist, 'track':track, 'contributors': contributors, 'etc': etc, 'sectype': '',
+                     'start' : -1, 'title': "", 'id': "",
+                     'ends': -1, 'mystart': -1, 'favourite':False, 'favourited':False, 'heard':False})
+
+    # data now needs checking - sometimes the time on tracks is zero, and needs to be interpolated.
+    pos = []
+    if (len(data) > 0):
+        ntr = len(data)
+        if data[0]['seconds'] == 0:
+            data[0]['seconds'] = 11
+        if data[0]['seconds'] == -1:
+            data[0]['seconds'] = 11
+        for i in range(len(data)):
+            pos.append(data[i]['seconds'])
+        pos.append(int(duration))
+        for i in range(len(data)):
+            if (pos[i] == -1):
+                j = i
+                while (pos[j] == -1):
+                    j = j + 1
+                step = (pos[j] - pos[i-1])/(j-(i-1))
+                for k in range(i,j):
+                    if pos[k] != -1:
+                        print "WARNING! pos[k]"
+                    pos[k] = pos[i-1] + (k-i+1)*step
+                    print "Adjusting position for "+str(k)+ " to " + str(pos[k])
+                    data[k]['seconds'] = pos[k]
+                    data[k]['sectype'] = 'interp'
+        for i in range(len(data)):
+            data[i]['seconds'] = pos[i]
     return data
 
 def get_episodes_audio():
@@ -256,6 +309,7 @@ if __name__ == "__main__":
     if DEBUG: 
         print 'Getting episodes' 
     episodes = get_episodes_metadata()
+# To skip episode parsing when developing:
 #    episodes = pickle.load(open(os.path.join(JB_DATABASE,"episodes.p"), "rb"))
 
     if DEBUG: 
@@ -264,5 +318,3 @@ if __name__ == "__main__":
     if DEBUG: 
         print 'Done.'
 
-# This is obsole now!
-#    parse_segment_files()
